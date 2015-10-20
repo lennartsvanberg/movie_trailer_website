3d1335f8e70d37a73270887b6d9eb5bb0afb86e7
Lennart Svanberg <lennartsvanberg@gmail.com>
2015-10-20 12:21:54 +0200

    The complete project

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # movie_trailer_website
+
+
+
+Udacity's 'P1 Movie Trailer' 
+This is my version of Udacity's 'P1 Movie Trailer' developed for the Full Stack Web Developer program. 
+
+It's a Movie Trailer Website where users can see some favorite movies of mine and watch the trailers.
+
+Quick Start
+Download the code and extract it
+Open your terminal 
+Go to the extracted folder
+Run the application: python entertainment_center.py
+If you hover over a movie the description is shown below
+Click on the Movie Title to see youtube trailer
+
+
+What's included
+Excluding this README.md file there are three python files:
+
+entertainment_center.py - The main file for this program. Builds a movie list and calls the method to generate the web page.
+fresh_tomatoes.py - Provides the web page generator. It takes a list of movies and builds the HTML to open up your default browser and displays an HTML page.
+media.py - Contains the media class that provides data structure to hold movie information.
+A fourth file, fresh_tomatoes.html, is generated when you run the program entertainment_center. This is the web page you  see in your browser.
+
+Creator
+
+lennartsvanberg.com
+
---
+++ entertainment_center.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Import the instance methods needed to run our website
+
+import fresh_tomatoes
+import media
+
+# Creating instance of our Movie class containing Title, Storyline, Poster image and Trailer URL of my favorite Movies
+
+sallskapsresan = media.Movie("Sällskapsresan",
+                             "Följ Stig-Helmer på hans resa till Kanarieöarna",
+                             "https://mz-prod.s3.amazonaws.com/uploads/photo/file/73087/original.jpg",
+                             "https://youtu.be/T4m2rBqTFDo")
+
+avatar = media.Movie ("Avatar",
+                      "A marine on an alien planet",
+                      "http://ecx.images-amazon.com/images/I/41vuODnDSuL.jpg",
+                      "https://youtu.be/5PSNL1qE6VY")
+
+blues_brothers = media.Movie ("Blues Brothers",
+                              "Follow the Blues Brothers on their journey to save a children's home",
+                              "http://cdn.traileraddict.com/content/universal-pictures/blues_brothers-4.jpg",
+                              "https://youtu.be/ma-rkV6wVJM")
+
+lord_of_the_rings = media.Movie ("Lord of the Rings",
+                             "Follow Frodo on his dangerous journey to destroy the Ring to rule them all.",
+                             "http://www.movieartarena.com/imgs/lotr1final.jpg",
+                             "https://youtu.be/V75dMMIW2B4")
+
+woman_in_red = media.Movie ("The Woman in Red",
+                           "Teddy Pierce falls in love with a Mysterious woman in Red threatening to destroy his long time marriage.",
+                           "https://70srichard.files.wordpress.com/2014/08/the-woman-in-red-movie-poster-1984-1020195918.jpg",
+                           "https://youtu.be/JDHhDanYAcQ")
+
+star_wars = media.Movie ("Star Wars",
+                        "An adventure far out in the sky many years from now!",
+                        "http://screenrant.com/wp-content/uploads/star-wars-episode-7-image.jpg",
+                        "https://youtu.be/vP_1T4ilm8M")
+
+movies = [sallskapsresan, avatar, blues_brothers, lord_of_the_rings, woman_in_red, star_wars]
+
+fresh_tomatoes.open_movies_page(movies)
+
+
